4180ef6b6da364b57bf3386a9235a801c9828093
Sudhir Kumar <102542436+sudhirk631@users.noreply.github.com>
2026-08-09 12:48:37 +0530

    Create setup.py for my_gan_package
    Set up package configuration for my_gan_package with dependencies.

---
+++ gan_model/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+ 
+
+setup(
+
+    name='my_gan_package',
+
+    version='0.1',
+
+    packages=find_packages(),
+
+    install_requires=[
+
+        'db-dtypes',
+
+        'tensorflow==2.19.0',
+
+        'pandas',
+
+        'numpy',
+
+        'scikit-learn',
+
+        'psutil',
+
+        'scipy',
+
+        'google-cloud-bigquery',
+
+        'gcsfs'
+
+    ],
+
+    include_package_data=True,
+
+    description='GAN for synthetic PNR data',
+
+)
